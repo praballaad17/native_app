8c8137101c4ce173b0cfc4b92efc3d7886c2b635
Prabal Laad <praballaad17@gmail.com>
2024-11-27 21:22:56 +0530

    removed env plugin from babel

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "nativewind/babel",
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env", // This is the correct moduleName value
-          path: ".env",
-          blocklist: null,
-          allowlist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
-    ],
+    plugins: ["nativewind/babel"],
   };
 };
